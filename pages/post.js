b26edf389ecf37e9cbe1af6a9f614750f67d909f
Yusuke Miyazaki <ysk1180@gmail.com>
2020-05-19 21:52:11 +0900

    add custom server ver

--- pages/post.js
+++ pages/post.js
@@ -1,4 +1,27 @@
-const Post = () => <h1>Post Page</h1>;
-Post.getIntialProps = async({ query }) = {};
+import axios from "axios";
+
+const Post = ({ id, comments }) => (
+  <div>
+    <h1>Post Comment for Post #{id}</h1>
+    {comments.map((comment) => (
+      <Comment {...comment} key={comment.id} />
+    ))}
+  </div>
+);
+
+const Comment = ({ email, body }) => (
+  <div>
+    <h5>{email}</h5>
+    <p>{body}</p>
+  </div>
+);
+
+Post.getInitialProps = async ({ query }) => {
+  const res = await axios.get(
+    `https://jsonplaceholder.typicode.com/comments?postId=${query.id}`
+  );
+  const { data } = res;
+  return { ...query, comments: data };
+};
 
 export default Post;
